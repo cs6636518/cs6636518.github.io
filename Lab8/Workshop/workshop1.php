6827c39f9ce2b94d5ad58ca04a9278f9048da81f
cs6636518 <s6604062636518@email.kmutnb.ac.th>
2025-09-14 15:26:24 +0700

    Lab8

--- Lab8/Workshop/workshop1.php
+++ Lab8/Workshop/workshop1.php
@@ -1,7 +1,11 @@
 <?php include "connect.php" ?>
 
 <html>
-<head><meta charset="utf-8"></head>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
 <body>
     <table border="1">
         <thead>
@@ -14,18 +18,19 @@
         </thead>
         <tbody>
             <?php
-                $stmt = $pdo->prepare("SELECT * FROM product");
-                $stmt->execute();
-                while ($row = $stmt->fetch()) {
-                    echo "<tr>";
-                    echo "<td>" . $row["pid"] . "</td>";
-                    echo "<td>" . $row["pname"] . "</td>";
-                    echo "<td>" . $row["pdetail"] . "</td>";
-                    echo "<td>" . $row["price"] . " บาท</td>";
-                    echo "</tr>\n";
-                }
+            $stmt = $pdo->prepare("SELECT * FROM product");
+            $stmt->execute();
+            while ($row = $stmt->fetch()) {
+                echo "<tr>";
+                echo "<td>" . $row["pid"] . "</td>";
+                echo "<td>" . $row["pname"] . "</td>";
+                echo "<td>" . $row["pdetail"] . "</td>";
+                echo "<td>" . $row["price"] . " บาท</td>";
+                echo "</tr>\n";
+            }
             ?>
         </tbody>
     </table>
 </body>
+
 </html>
--- Lab8/connect.php
+++ Lab8/connect.php
@@ -6,4 +6,3 @@ try {
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-?>
