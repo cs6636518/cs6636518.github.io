379be844393cd193aaa5843b21d725636833c118
cs6636518 <s6604062636518@email.kmutnb.ac.th>
2025-09-22 15:46:54 +0700

    Lab8

--- Lab8/Workshop/5member_detail.php
+++ Lab8/Workshop/5member_detail.php
@@ -7,23 +7,19 @@
 
 <body>
     <?php
-    // 1. กำหนดคำสั่ง SQL ให้ดึงสนค้าตามรหัสสินค้า
-
     $stmt = $pdo->prepare("SELECT * FROM member WHERE username = ?");
-    $stmt->bindParam(1, $_GET["username"]); // 2. นำค่า pid ที่ส่งมากับ URL กำหนดเป็นเงื่อนไข
-    $stmt->execute(); // 3. เริ่มค้นหา
-    $row = $stmt->fetch(); // 4. ดึงผลลัพธ์ (เนื่องจากมีข้อมูล 1 แถวจึงเรียกเมธอด fetch เพียงครั้งเดียว)
+    $stmt->bindParam(1, $_GET["username"]);
+    $stmt->execute();
+    $row = $stmt->fetch();
     ?>
-
     <div style="display:flex">
         <div>
             <img src='member_photo/<?= $row["username"] ?>.jpg' width='200'>
         </div>
         <div style="padding: 15px">
-            <h2><?= $row["username"] ?></h2>
-            ชื่อ : <?= $row["name"] ?><br>
-            ที่อยู่ : <?= $row["address"] ?><br><br>
-            อีเมล : <?= $row["email"] ?>
+            ชื่อสมาชิก: <?= $row["name"] ?><br>
+            ที่อยู่: <?= $row["address"] ?><br>
+            อีเมล์: <?= $row["email"] ?>
         </div>
     </div>
 </body>
--- Lab8/Workshop/7insert_member.php
+++ Lab8/Workshop/7insert_member.php
@@ -1,17 +1,17 @@
 <?php include "connect.php" ?>
 
 <?php
-$stmt = $pdo->prepare("INSERT INTO member VALUES (?, ?, ?, ?, ?, ?)");
-$stmt->bindParam(1, $_POST["username"]);
-$stmt->bindParam(2, $_POST["password"]);
-$stmt->bindParam(3, $_POST["name"]);
-$stmt->bindParam(4, $_POST["address"]);
-$stmt->bindParam(5, $_POST["mobile"]);
-$stmt->bindParam(6, $_POST["email"]);
-$stmt->execute(); // เริ่มเพิ่มข้อมูล
-$pid = $pdo->lastInsertId(); // ขอคีย์หลักที่เพิ่มสำเร็จ
+    $stmt = $pdo->prepare("INSERT INTO member VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bindParam(1, $_POST["username"]);
+    $stmt->bindParam(2, $_POST["password"]);
+    $stmt->bindParam(3, $_POST["name"]);
+    $stmt->bindParam(4, $_POST["address"]);
+    $stmt->bindParam(5, $_POST["mobile"]);
+    $stmt->bindParam(6, $_POST["email"]);
+    $stmt->execute(); // เริ่มเพิ่มข้อมูล
+    //$pid = $pdo->lastInsertId(); // ขอคีย์หลักที่เพิ่มสำเร็จ
 
-$username = $_POST["username"];
+    $username = $_POST["username"];
 ?>
 <html>
 
@@ -20,7 +20,7 @@ $username = $_POST["username"];
 </head>
 
 <body>
-    เพิ่มสมาชิกสำเร็จ สมาชิกใหม่ คือ <?= $username ?>
+    เพิ่มสมาชิกสำเร็จ username คือ <?= $username ?>
 </body>
 
 </html>
